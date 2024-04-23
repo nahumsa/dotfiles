90111b20f83958c049067aabea72b48f094ee73e
Nahum Sá <nahumsaa@gmail.com>
2024-04-22 23:13:46 -0300

    feat: add telescope to harpoon list

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -273,7 +273,26 @@ return {
         "<leader>a",
         function()
           local harpoon = require("harpoon")
-          harpoon.ui:toggle_quick_menu(harpoon:list())
+
+          local conf = require("telescope.config").values
+          local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers")
+              .new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                  results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+              })
+              :find()
+          end
+          toggle_telescope(harpoon:list())
         end,
         desc = "harpoon quick menu",
       },
